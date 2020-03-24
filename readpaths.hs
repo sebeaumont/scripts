dce9b371371788f316962d74f02ee9db8cfebb8d
Simon Beaumont <datalligator@icloud.com>
2020-03-24 11:14:55 +0000

    Added readpaths to create path from file -- kind of useless

---
+++ readpaths.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE LambdaCase #-}
+module Main
+  where
+
+import Data.List
+import System.Environment
+import System.IO.Error
+import Control.Exception (try, Exception)
+
+-- | Read the lines if any error we give up
+readlines :: FilePath -> IO (Either IOError [String])
+readlines p = try $ (return . lines =<< readFile p)
+
+-- | Cant believe this is not in the prelude
+splitWhen :: (Char -> Bool) -> String -> [String]
+splitWhen p s =
+  case dropWhile p s of
+    "" -> []
+    s' -> w : splitWhen p s''
+      where (w, s'') = break p s'
+  
+-- | Split up the path variable
+splitPath :: String -> [String]
+splitPath = splitWhen (==':')
+
+-- | Make a delimited path 
+makePath :: [String] -> String
+makePath = intercalate ":" . nub 
+
+-- | Jolly wee strings
+home, path :: String
+home = "HOME" 
+path = "PATH"
+
+-- | Let's go
+main :: IO ()
+main = do
+  h <- getEnv home
+  p <- getEnv path
+  -- print out new path or just the exisiting environment
+  -- if things are sinister...
+  readlines (h ++ "/.paths") >>= \case
+    Left e -> putStr $ p 
+    Right l -> putStr $ makePath l  
+    
+
