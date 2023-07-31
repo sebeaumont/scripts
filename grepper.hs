af5cb524377cc0df3db715ea45176936ddc22931
Simon Beaumont <simon.beaumont@tracsis.com>
2023-07-31 10:38:40 +0100

    dryrun for find arguments in grepper command

--- grepper.hs
+++ grepper.hs
@@ -1,3 +1,4 @@
+
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE DeriveDataTypeable #-}
@@ -16,6 +17,7 @@ data Grep = Grep
   , glob :: String
   , search :: String
   , grepflags :: [String]
+  , dryrun :: Bool
   } deriving (Show, Data, Typeable)
 
 grep = Grep
@@ -23,20 +25,27 @@ grep = Grep
   , prune = [".stack*", ".hg", ".git", "dist*"] &= typDir &= help "Directory trees to prune"
   , search = "" &= help "pattern to grep for"
   , glob = "*.hs" &= help "file glob to search into"
-  , grepflags = ["--color", "-H", "-b", "-i"] &= help "flags for grep"
+  -- seb removed "-i" from here - prefer case sensitive grep 
+  , grepflags = ["--color", "-H", "-b"] &= help "flags for grep"
+  , dryrun = False &= help "print the command only"
   }
   
 
 main :: IO ()
 main = do
   args <- cmdArgs grep
-  shelly $ grepper_
-    (T.pack $ root args)
-    (T.pack $ glob args)
-    (T.pack $ search args)
-    [T.pack g | g <- grepflags args]
-    [T.pack p | p <- prune args]
+  let findargs = grepper
+        (T.pack $ root args)
+        (T.pack $ glob args)
+        (T.pack $ search args)
+        [T.pack g | g <- grepflags args]
+        [T.pack p | p <- prune args]
 
+  if dryrun args
+    then putStrLn ("find " ++ show findargs)
+    else shelly $ run_ "find" $ findargs
+
+  
 -- | excluded paths from starting dir
 excluded :: [T.Text]
 excluded = [".stack*", ".hg", ".git", "dist*"]
@@ -45,16 +54,15 @@ exclude :: [T.Text] -> [T.Text]
 exclude [] = []
 exclude (p:ps) = ["-o", "-name", p] ++ exclude ps
 
-grepper_ :: T.Text -> T.Text -> T.Text -> [T.Text] -> [T.Text] -> Sh ()
-grepper_ dir pat term grepf (e:es) =
-  run_ "find" $ concat [[dir, "-type", "d", "(", "-name", e],
-                         exclude es,
-                         [")", "-prune", "-o",
-                          "-type", "f", "-name", pat, "-exec", "grep"],
-                         grepf,
-                         ["-b", "-i", term, "{}", ";"]
-                       ]
-
+grepper :: T.Text -> T.Text -> T.Text -> [T.Text] -> [T.Text] -> [T.Text]
+grepper dir pat term grepf (e:es) =
+  concat [[dir, "-type", "d", "(", "-name", e],
+           exclude es,
+           [")", "-prune", "-o",
+             "-type", "f", "-name", pat, "-exec", "grep"],
+           grepf,
+           ["-b", "-i", term, "{}", ";"]
+         ]
 
 {-
 ,"-exec", "echo", "-n", "\ESC[32m", ";",
